94f8a7c5fbf453a48511a85663393d36a284b4e0
Manu07Alvarez <manu_07_Alvarez@hotmail.com>
2024-08-20 18:59:48 -0300

    starting

---
+++ administrationapp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace administrationapp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
